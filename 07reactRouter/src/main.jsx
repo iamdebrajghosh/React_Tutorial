62a2c6bc0cc9ad35bd2a319c492d56c3ae5405a2
DebrajGhosh <debrajghosh774@gmail.com>
2026-01-07 19:47:38 +0530

    Day 11: React Router Crash Cource

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,47 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Home/Home.jsx'
+import About from './About/About.jsx'
+import Contact from './Contact/Contact.jsx'
+import User from './User/User.jsx'
+import Github, { githubInfoLoader } from './Github/Github.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "user/:userid",
+        element: <User />
+      },
+      {
+        loader: githubInfoLoader,
+        path: "github",
+        element: <Github />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
