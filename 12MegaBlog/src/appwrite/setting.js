508e8d81ca73282c53c0d47a146f8175b01dc0b0
DebrajGhosh <debrajghosh774@gmail.com>
2026-01-16 16:09:09 +0530

    Day 21: Build authentication service with appwrite in React Project

---
+++ 12MegaBlog/src/appwrite/setting.js
@@ -0,0 +1,129 @@
+import config from "../config/config";
+import { Client, ID, Account, Databases, Storage, Query } from "appwrite";
+
+export class AppwriteService {
+    client = new Client();
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.storage = new Storage(this.client);
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false;
+        }
+    }
+    // file upload sevice
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false;
+        }
+    }
+    // delete file service
+    async deleteFile(fileId) {
+        try {
+            await this.storage.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false;
+        }
+    }
+    // get file preview
+    getFilePreview(fileId) {
+        return this.storage.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        );
+    }
+
+}
+
+const appwriteService = new AppwriteService();
+
+export default appwriteService;
